d772dcd8d56fa95f00af00d68ffba6339220eaa2
MallocGab <gabriel.padis@laposte.net>
2019-03-11 16:58:17 +0100

    start activity

--- app/src/main/java/moi/android/fr/tennis_tracker/MainActivity.java
+++ app/src/main/java/moi/android/fr/tennis_tracker/MainActivity.java
@@ -1,5 +1,6 @@
 package moi.android.fr.tennis_tracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -74,16 +75,28 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         switch (item.getItemId()) {
             case android.R.id.home:
                 drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            case R.id.action_settings:
+                return true;
+
+            case R.id.main_activity:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.local_results_activity:
+                Intent intent2 = new Intent(this, local_results.class);
+                startActivity(intent2);
+                return true;
+
+            case R.id.remote_results_activity:
+                Intent intent3 = new Intent(this, remote_results.class);
+                startActivity(intent3);
                 return true;
         }
 
